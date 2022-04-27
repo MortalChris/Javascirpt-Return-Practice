000191b1f458460ae395d15fc6498a187b459d0f
Christian V <88056810+MortalChris@users.noreply.github.com>
2022-04-27 14:58:49 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const number = 7
+const numberTwo = 6
+
+const add = function(n1, n2){
+  return(n1 + n2);
+};
+const mult = function (n1, n2){
+  return(n1 * n2);
+};
+
+const sum = add(number, numberTwo);
+const finalSum = mult(prompt("Enter Number here") ,sum);
+console.log(finalSum);
+
+//Return Practice
